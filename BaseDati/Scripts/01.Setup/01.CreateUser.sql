dfdd3296f6254621eb256340e44f29843d9ed8af
fdipierno <f.dipierno@studenti.unina.it>
2023-12-27 18:54:26 +0100

    prima commit, scripts db di setup.

---
+++ BaseDati/Scripts/01.Setup/01.CreateUser.sql
@@ -0,0 +1,13 @@
+create user uninadev identified by Unidevtest4010$;
+
+grant create session to uninadev;
+
+grant create table to uninadev;
+
+grant create view to uninadev;
+
+grant create view to uninadev;
+
+grant create sequence to uninadev;
+
+grant unlimited tablespace to uninadev;
