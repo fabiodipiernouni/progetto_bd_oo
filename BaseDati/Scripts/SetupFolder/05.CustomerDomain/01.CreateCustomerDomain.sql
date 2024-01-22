6cb15dd673478c035b9f95755853b44af115a2a0
Giuseppe Di Martino <giuseppedimartin_02@libero.it>
2024-01-22 01:28:40 +0100

    WIP, reso naming coerente con gli altri scripts

--- BaseDati/Scripts/SetupFolder/05.CustomerDomain/01.CreateCustomerDomain.sql
+++ BaseDati/Scripts/SetupFolder/05.CustomerDomain/01.CreateCustomerDomain.sql
@@ -1,18 +1,18 @@
 begin
-    execute immediate 'DROP TABLE "Cliente" CASCADE CONSTRAINTS PURGE';
+    execute immediate 'DROP TABLE Cliente CASCADE CONSTRAINTS PURGE';
 exception when others then null;
 end;
 
-create table "Cliente"
+create table Cliente
 (
-    IDCliente int not null,
-    Nome      VARCHAR2(64 byte) not null, -- se valorizzato RagioneSociale rappresenta il nome del referente
-    Cognome   VARCHAR2(64 byte) not null, -- se valorizzato RagioneSociale rappresenta il cognome del referente
+    IdCliente integer not null,
+    Nome VARCHAR2(64 byte) not null, -- se valorizzato RagioneSociale rappresenta il nome del referente
+    Cognome VARCHAR2(64 byte) not null, -- se valorizzato RagioneSociale rappresenta il cognome del referente
     RagioneSociale VARCHAR2(64 byte),
-    Email    VARCHAR2(64 byte) not null unique,
+    Email VARCHAR2(64 byte) not null unique,
     CodiceFiscale VARCHAR2(16 byte),
-    partitaIVA VARCHAR2(16 byte),
-    constraint "Cliente_PK" primary key ("IDCliente")
+    PartitaIVA VARCHAR2(16 byte),
+    constraint pk_Cliente primary key ("IdCliente")
 );
 
 begin
@@ -22,30 +22,25 @@ end;
 
 create table OrdineCliente
 (
-    ID int not null,
+    ID integer not null,
     DataOrdine DATE not null,
     ImportoTotale NUMBER(10,2) not null,
     Stato VARCHAR2(64 byte) not null, -- Stato dell'ordine: Confermato, Spedito, Consegnato, ResoInCorso, Reso, Annullato
-    IDCliente int not null,
-    IdIndirizzoFatturazione int not null,
-    IdIndirizzoSpedizione int, -- valorizzato se diverso da indirizzo di fatturazione
-    constraint "OrdineCliente_PK" primary key ("IDOrdine"),
-    constraint "OrdineCliente_Stato_CK" check ("Stato" in ('Confermato', 'Spedito', 'Consegnato', 'ResoInCorso', 'Reso', 'Annullato')),
-    constraint "OrdineCliente_Cliente_FK" foreign key ("IDCliente") references "Cliente" ("IDCliente"),
-    constraint "OrdineCliente_IndirizzoFatturazione_FK" foreign key ("IdIndirizzoFatturazione") references "Indirizzo" ("Id"),
-    constraint "OrdineCliente_IndirizzoSpedizione_FK" foreign key ("IdIndirizzoSpedizione") references "Indirizzo" ("Id")
+    IdCliente integer not null,
+    IdIndirizzoFatturazione integer not null,
+    IdIndirizzoSpedizione integer, -- valorizzato se diverso da indirizzo di fatturazione
+    constraint pk_OrdineCliente primary key (IdOrdine),
+    constraint check_OrdineCliente_Stato check (Stato in ('Confermato', 'Spedito', 'Consegnato', 'ResoInCorso', 'Reso', 'Annullato')),
+    constraint fk_OrdineCliente_IdCliente foreign key (IdCliente) references Cliente (IdCliente),
+    constraint fk_OrdineCliente_IdIndirizzoFatturazione foreign key (IdIndirizzoFatturazione) references Indirizzo (Id),
+    constraint fk_OrdineCliente_IdIndirizzoSpedizione foreign key (IdIndirizzoSpedizione) references Indirizzo (Id)
 );
 
 create table DettaglioOrdine (
-    IDOrdine int not null,
-    IDProdotto int not null,
+    IdOrdine int not null,
+    IdProdotto int not null,
     Quantita int not null,
-    constraint "DettaglioOrdine_PK" primary key ("IDOrdine", "IDProdotto"),
-    constraint "DettaglioOrdine_OrdineCliente_FK" foreign key ("IDOrdine") references "OrdineCliente" ("Id"),
-    constraint "DettaglioOrdine_Prodotto_FK" foreign key ("IDProdotto") references "CatalogoProdotti" ("Id")
+    constraint pk_DettaglioOrdine primary key (IdOrdine, IdProdotto),
+    constraint fk_DettaglioOrdine_IdOrdine foreign key (IdOrdine) references OrdineCliente (IdOrdine),
+    constraint fk_DettaglioOrdine_IdProdotto foreign key (IdProdotto) references CatalogoProdotti (Id)
 );
-
-
-
-
-
