e9c27c60788ae566d29847b054f947b3d7e012a9
fdipierno <f.dipierno@studenti.unina.it>
2024-03-06 18:35:56 +0100

    remove any file

--- ObjectOrientation/uninadelivery/uninadelivery-entity/src/main/java/module-info.java
+++
@@ -1,6 +0,0 @@
-module org.uninadelivery.entity {
-    requires lombok;
-
-    exports org.uninadelivery.entity.orgdomain;
-    exports org.uninadelivery.entity.appdomain;
-}
--- ObjectOrientation/uninadelivery33/src/main/java/org/unina/uninadelivery33/dal/factory/GruppoCorriereDAO.java
+++
@@ -1,8 +0,0 @@
-package org.unina.uninadelivery33.dal.factory;
-
-import org.unina.uninadelivery33.dal.exception.PersistenceException;
-import org.unina.uninadelivery33.entity.orgdomain.GruppoCorriereDTO;
-
-public interface GruppoCorriereDAO {
-    GruppoCorriereDTO selectById(long id) throws PersistenceException;
-}
