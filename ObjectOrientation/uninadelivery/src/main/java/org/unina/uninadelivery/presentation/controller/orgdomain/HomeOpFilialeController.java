c94d9682612ae310c7b941dd37aa7567f17755d9
fdipierno <f.dipierno@studenti.unina.it>
2024-03-09 10:29:42 +0100

    wip

--- ObjectOrientation/uninadelivery/src/main/java/org/unina/uninadelivery/presentation/controller/orgdomain/HomeOpFilialeController.java
+++ ObjectOrientation/uninadelivery/src/main/java/org/unina/uninadelivery/presentation/controller/orgdomain/HomeOpFilialeController.java
@@ -1,9 +1,25 @@
 package org.unina.uninadelivery.presentation.controller.orgdomain;
 
+import javafx.fxml.FXML;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
 public class HomeOpFilialeController {
-    public HomeOpFilialeController(Stage dashboardStage) {
+    @FXML
+    public AnchorPane mainAnchorPane;
 
+    public HomeOpFilialeController(Stage dashboardStage) {
+        StackPane stackPane = (StackPane) dashboardStage.getScene().getRoot().lookup("#contentPane");
+        double anchorPaneWidth = stackPane.getWidth();
+        double anchorPaneHeight = stackPane.getHeight();
+
+        double controlWidth = mainAnchorPane.getWidth();
+        double controlHeight = mainAnchorPane.getHeight();
+
+        AnchorPane.setTopAnchor(mainAnchorPane, (anchorPaneHeight - controlHeight) / 2);
+        AnchorPane.setBottomAnchor(mainAnchorPane, (anchorPaneHeight - controlHeight) / 2);
+        AnchorPane.setLeftAnchor(mainAnchorPane, (anchorPaneWidth - controlWidth) / 2);
+        AnchorPane.setRightAnchor(mainAnchorPane, (anchorPaneWidth - controlWidth) / 2);
     }
 }
